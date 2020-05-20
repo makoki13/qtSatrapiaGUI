bfb1c7de86b6a744ea088e81d3bb57c43afc9a8c
makoki <pablo.makoki@gmail.com>
2020-05-20 14:18:17 +0200

    otro test.bat

--- sube.bat
+++ sube.bat
@@ -1,14 +1,14 @@
 @echo off
 set /p comentario=Comentario:
+echo "Subiendo GUI"
 cd C:\Qt\proyectos\qtSatrapiaGUI\
 git add .
 git commit -m "%comentario%"
 git push origin master
-pause
+echo "Subiendo Clases"
 cd C:\Qt\proyectos\QtSatrapia\
 git add .
 git commit -m "%comentario%"
 git push origin master
 echo "fin!"
-pause
 exit
