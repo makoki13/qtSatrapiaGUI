a3858ee7ecdb1fb4e6bb441d561ff17c6f6c1035
pablo <pablo.makoki@gmail.com>
2020-05-20 19:12:53 +0200

    test local

---
+++ localbaja.bat
@@ -0,0 +1,9 @@
+@echo off
+echo "Bajando GUI"
+cd D:\home\Makoki\qt\proyectos\qtSatrapiaGUI\
+git pull
+echo "Bajando Clases"
+cd D:\home\Makoki\qt\proyectos\QtSatrapia\
+git pull origin master
+echo "fin!"
+exit
---
+++ localsube.bat
@@ -0,0 +1,14 @@
+@echo off
+set /p comentario=Comentario:
+echo "Subiendo GUI"
+cd D:\home\Makoki\qt\proyectos\qtSatrapiaGUI\
+git add .
+git commit -m "%comentario%"
+git push origin master
+echo "Subiendo Clases"
+cd D:\home\Makoki\qt\proyectos\QtSatrapia\
+git add .
+git commit -m "%comentario%"
+git push origin master
+echo "fin!"
+exit
